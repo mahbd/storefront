6504bada53ae8d5d9f0b53ed7b934a218f8b3f50
Mahmudul Alam <mahmudula2000@gmail.com>
2021-10-02 19:54:38 +0600

    Use of Q object

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q
 
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from store.models import Product
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    query_set = Product.objects.filter(
+        Q(inventory__lt=10) | ~Q(unit_price__lt=20))
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
