fbd94bc9a2baf67c9ef95d8c0d7b2811318a88ca
Mahmudul Alam <mahmudula2000@gmail.com>
2021-10-02 19:57:06 +0600

    uses of F object

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
 
 from store.models import Product
 
 
 def say_hello(request):
+    query_set = Product.objects.filter(inventory=F('unit_price'))
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
+
+
+def say_hello_q(request):
     query_set = Product.objects.filter(
         Q(inventory__lt=10) | ~Q(unit_price__lt=20))
     return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
